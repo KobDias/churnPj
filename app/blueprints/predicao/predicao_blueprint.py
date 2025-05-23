5e1380e668a3132205f96b881efa444324622a46
KobDias <sofhia.dias@etec.sp.gov.br>
2025-05-23 10:18:49 -0300

    RODANDO

--- app/blueprints/predicao/predicao_blueprint.py
+++ app/blueprints/predicao/predicao_blueprint.py
@@ -6,7 +6,8 @@ from db import db
 from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
-from .processamento import predict, processo
+from .processamento import gerar_df_pred, grupos_pred, processa_modelo, preprocess_input, gerar_grafico
+import joblib
 from models import Documentos
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -17,28 +18,6 @@ predicao_bp = Blueprint('predicao', __name__, template_folder='templates', url_p
 ALLOWED_EXTENSIONS = set(['csv'])
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def gerar_grafico(df, nome):
-
-    data = pd.read_csv(df)
-
-    if 'SalePrice' not in data.columns:
-        print(f"Coluna 'SalePrice' não encontrada em {df}. Gráfico não será gerado.")
-        return
-
-    # Exemplo de gráfico: histograma da coluna 'SalePrice'
-    plt.figure(figsize=(10, 6))
-    data['SalePrice'].hist(bins=30)
-    plt.title('Distribuição do SalePrice')
-    plt.xlabel('SalePrice')
-    plt.ylabel('Frequência')
-
-    # Salva o gráfico em um buffer
-    graph_dir = os.path.join('app', 'static', 'uploads', 'sys', 'graphs')
-    os.makedirs(graph_dir, exist_ok=True)
-    plt.savefig(os.path.join(graph_dir, f'{nome}.png'), format='png')
-
-
 
 @predicao_bp.route('/upload', methods=['GET', 'POST'])
 @login_required
@@ -55,16 +34,17 @@ def upload():
             filename = secure_filename(file.filename)
         
 
-            upload_folder = os.path.abspath(
-            os.path.join(current_app.root_path, 'static', 'uploads', 'user', 'original'))
+            upload_folder = os.path.join('app', 'static', 'uploads', 'user', 'original')
+            print(upload_folder)
             os.makedirs(upload_folder, exist_ok=True)  # Garante que o diretório existe
             file_path = os.path.join(upload_folder, filename)
             file.save(file_path)
+            new_filename = filename.split('.csv', 1)[0]
             
             uploadDoc = Documentos(
                 user_id=current_user.id,
                 caminho_origem=file_path,
-                nome_documento=filename
+                nome_documento=new_filename
             )
             db.session.add(uploadDoc)
             db.session.commit()
@@ -80,21 +60,43 @@ def upload():
 @login_required
 def views(id):
     doc = db.session.query(Documentos).filter_by(id=id).first()
-    caminho = url_for('static', filename=f'uploads/user/original/{doc.nome_documento}')
-    nome = doc.nome_documento.split('_202', 1)[0]
+    caminho = doc.caminho_origem
+    nome = doc.nome_documento.split('.csv', 1)[0]
     if not doc:
         return 'Documentos não encontrado', 404
     predito = doc.caminho_pred
     if request.method == 'POST':
-        predicao = predict(caminho)
+        print("submittaram")
+        input_df = pd.read_csv(caminho)
+        data = joblib.load('app/blueprints/predicao/modelo_churn_rf_bank_telco.pkl')
+        model = data['model']
+        columns = data['columns']
 
-        predito = processo(caminho, predicao, nome)
+        X = preprocess_input(input_df, columns)
+
+        preds = model.predict(X)
+        probs = model.predict_proba(X)[:, 1]
+
+        df_pred = gerar_df_pred(X, preds, probs)
+        pred_path = os.path.join('app', 'static', 'uploads', 'sys', 'pred', f'{nome}_pred.csv')
+        df_pred.to_csv(pred_path, index=False)
+        doc.caminho_pred = pred_path        
+
+        agrupado = grupos_pred(df_pred, nome)
+        # ... seu código de agrupamento ...
+        agrupado.columns = ['Sexo', 'Faixa Etária', 'Risco', 'Probabilidade Média', 'Total Clientes', 'Cancelamentos']
+        agrupado['Probabilidade Média (%)'] = (agrupado['Probabilidade Média'] * 100).round(2)
+        agrupado = agrupado.drop(columns=['Probabilidade Média'])
+
+        # Salvar para download
+        agrupado.to_csv(f'app/static/uploads/sys/pred/{nome}_grupos_risco.csv', index=False)
+
         db.session.commit()
-        # add DATA DE PREDICAO se possivel pra ver a att
-        return redirect(url_for('predicao.views', id=doc.id)) #redireciona para a view do documento
+
+        gerar_grafico(df_pred, nome, model, columns)
+        return redirect(url_for('predicao.views', id=doc.id, df=df_pred)) #redireciona para a view do documento
     #get
     print(caminho)
-    gerar_grafico(doc.caminho_origem, nome)
     fig = url_for('static', filename=f'uploads/sys/graphs/{nome}.png')
     if fig:
         return render_template('view.html', fig=fig, doc=doc, predito=predito, nome=nome, caminho=caminho)
--- app/blueprints/predicao/processamento.py
+++ app/blueprints/predicao/processamento.py
@@ -2,64 +2,106 @@ import joblib
 import pandas as pd
 import os
 
-def processo(csv_path, y_pred, nome):
-    df = pd.read_csv(csv_path)
-    df['SalePrice'] = y_pred
+# Defs de dataset
 
-    output_dir = os.path.join('app', 'static', 'uploads', 'preditos')
-    os.makedirs(output_dir, exist_ok=True)
+def classificar_risco(prob):
+    if prob >= 0.7:
+        return 'Alto'
+    elif prob >= 0.4:
+        return 'Médio'
+    else:
+        return 'Baixo'
+def gerar_df_pred(X, preds, probs):
+    """
+    X_input: DataFrame com as colunas usadas no treinamento (após preprocessamento e alinhamento)
+    preds: array ou lista com as predições (0/1)
+    probs: array ou lista com as probabilidades de churn (float)
+    """
+    df_pred = X.copy()
+    df_pred['Churn'] = preds
+    df_pred['Probabilidade'] = probs
+    df_pred['Risco'] = df_pred['Probabilidade'].apply(classificar_risco)
+    return df_pred
 
-    # Usa o nome base do arquivo original para criar o novo nome
-    output_name = f"{nome}_predito.csv"
-    caminhoPredito = os.path.join(output_dir, output_name)
-    df.to_csv(caminhoPredito, index=False)
-    return caminhoPredito
+# Função de grafico
+def gerar_grafico(df_pred, nome, model, columns):
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    importance = model.feature_importances_
+    feat_importances = pd.DataFrame({
+        'feature': columns,
+        'importance': (importance*100).round(2)
+    })
+
+    feat_importances = feat_importances[feat_importances['importance'] > 0]
+    feat_importances = feat_importances.sort_values(by='importance', ascending=False).head(10)
+
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='importance', y='feature', data=feat_importances)
+    plt.title('Importância das Features')
+    plt.xlabel('Importância (%)')
+    plt.ylabel('Features')
+    plt.tight_layout()
+    fig_path_Importance = os.path.join('app', 'static', 'uploads', 'sys', 'graphs', f'{nome}_importante.png')
+    plt.savefig(fig_path_Importance)
+    plt.close()
+
+    if 'Risco' in df_pred.columns:
+        plt.figure(figsize=(8, 5))
+        sns.countplot(x='Risco', data=df_pred, palette='viridis')
+        plt.title('Distribuição dos Grupos de Risco')
+        plt.xlabel('Risco')
+        plt.ylabel('Quantidade')
+        plt.tight_layout()
+        fig_path2 = os.path.join('app', 'static', 'uploads', 'sys', 'graphs', f'{nome}_riscos.png')
+        plt.savefig(fig_path2)
+        plt.close()
+    
 
 # Função para pré-processar um DataFrame
-def preprocess(df):
-    # Define um limite para colunas com muitos valores ausentes (mais de 50% de nulos serão removidas)
-    limiar_nulos = 0.5
-    df = df.loc[:, df.isnull().mean() < limiar_nulos]
 
-    # Preenche valores ausentes nas colunas numéricas com a mediana de cada coluna
-    num_cols = df.select_dtypes(include=np.number).columns
-    for col in num_cols:
-        df[col].fillna(df[col].median(), inplace=True)
-
-    # Preenche valores ausentes nas colunas categóricas com a moda (valor mais frequente)
-    cat_cols = df.select_dtypes(include='object').columns
-    for col in cat_cols:
-        df[col].fillna(df[col].mode()[0], inplace=True)
-
-   #pd.get_dummies()	Converte colunas de texto em colunas numéricas binárias
-   #drop_first=True	Remove a primeira dummy de cada categoria para evitar redundância
-    df = pd.get_dummies(df, drop_first=True)
-
+def preprocess_input(df, columns):
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = pd.to_numeric(df[col], errors='coerce')
+        cat_cols = df.select_dtypes(include='object').columns
+        df = pd.get_dummies(df, columns=cat_cols, drop_first=True)
+        df = df.reindex(columns=columns, fill_value=0)
     return df
 
-def predict(csv_path):
-    modelo = joblib.load('app\blueprints\predicao\static\model\model.pkl')
-    df = pd.read_csv(csv_path)
-    
-    # Seleciona as colunas usadas no treinamento
-    features = ["OverallQual", "GrLivArea", "GarageCars", "SaleCondition"]
-    missing = [col for col in features if col not in df.columns]
-    if missing:
-        raise ValueError(f"Colunas faltando no CSV: {', '.join(missing)}")
-    
-    X = df[features].copy()
-    sale_condition_map = {
-        'Normal': 0,
-        'Abnorml': 1,
-        'AdjLand': 2,
-        'Alloca': 3,
-        'Family': 4,
-        'Partial': 5
-    }
-    X["SaleCondition"] = X["SaleCondition"].map(sale_condition_map)
-    X["SaleCondition"] = X["SaleCondition"].fillna(-1)  # ou .dropna() se preferir remover
-    xEscalado = scaler.transform(X)
+def processa_modelo(input_df):
+    data = joblib.load('modelo_churn_rf_bank_telco.pkl')
+    model = data['model']
+    columns = data['columns']
+    X = preprocess_input(input_df, columns)
+    return model.predict(X)
 
-    y_pred = modelo.predict(xEscalado)
-    return y_pred
+def grupos_pred(df_pred, nome):
+    # Detectar colunas de sexo e idade
+    col_genero = [col for col in df_pred.columns if 'gender' in col.lower()]
+    col_idade = [col for col in df_pred.columns if 'age' in col.lower()]
 
+    if col_genero:
+        df_pred['Sexo'] = df_pred[col_genero[0]].apply(lambda x: 'Homem' if x == 1 else 'Mulher')
+    else:
+        df_pred['Sexo'] = 'Desconhecido'
+    if col_idade:
+        df_pred['Idade'] = df_pred[col_idade[0]].astype(int)
+        df_pred['Faixa Etária'] = pd.cut(df_pred['Idade'], bins=[17, 30, 45, 60, 120],
+                                         labels=['18–30', '31–45', '46–60', '60+'])
+    else:
+        df_pred['Faixa Etária'] = 'Desconhecida'
+
+    # Agrupar por Sexo, Faixa Etária e Risco
+    agrupado = df_pred.groupby(['Sexo', 'Faixa Etária', 'Risco']).agg({
+    'Probabilidade': 'mean',
+    'Churn': 'count'
+    }).reset_index()
+    agrupado.columns = ['Sexo', 'Faixa Etária', 'Risco', 'Probabilidade Média', 'Total Clientes']
+    agrupado['Probabilidade Média (%)'] = (agrupado['Probabilidade Média'] * 100).round(2)
+    agrupado = agrupado.drop(columns=['Probabilidade Média'])
+
+    # Salvar para download
+    agrupado.to_csv(f'app/static/uploads/sys/pred/{nome}_grupos_risco.csv', index=False)
+    return agrupado
